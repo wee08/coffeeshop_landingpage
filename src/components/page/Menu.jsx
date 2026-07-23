b92aaa0ae60b3a3d3b2b99f1ca41eaa13005ad41
wee08 <manutnithyasean@gmail.com>
2026-07-23 14:34:14 +0700

    import assets

--- src/components/page/Menu.jsx
+++ src/components/page/Menu.jsx
@@ -1,6 +1,20 @@
 import Title from "../Title";
-import hotlatte from "../../assets/img/hot_latte.png";
+import latte from "../../assets/img/hot_latte.png";
+import americano from "../../assets/img/hot_americano.png";
+import choco from "../../assets/img/hot_choco.png";
+import lemon from "../../assets/img/hot_lemon.png";
+import milk from "../../assets/img/hot_milk.png";
+import matcha from "../../assets/img/hot_matcha.png";
 const Menu = () => {
+  const drinks = [
+    { src: latte, alt: "hot latte" },
+    { src: choco, alt: "hot chocolate" },
+    { src: americano, alt: "hot americano" },
+    { src: lemon, alt: "hot lemon" },
+    { src: milk, alt: "hot milk" },
+    { src: matcha, alt: "hot matcha" },
+  ];
+
   return (
     <>
       <Title
@@ -9,7 +23,13 @@ const Menu = () => {
         w={"auto"}
         text={"center"}
       />
-      <img src={hotlatte} alt="" />
+      <div className="carousel rounded-box">
+        {drinks.map((i, idx) => (
+          <div className="carousel-item" key={idx}>
+            <img className="w-117.5" src={i.src} alt="" />
+          </div>
+        ))}
+      </div>
     </>
   );
 };
