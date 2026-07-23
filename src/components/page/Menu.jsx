cd37edcc01a9a0be5b3cedf61dd001a5cd9f9fa9
wee08 <manutnithyasean@gmail.com>
2026-07-23 15:28:50 +0700

    add handle index to chevron icon

--- src/components/page/Menu.jsx
+++ src/components/page/Menu.jsx
@@ -6,7 +6,7 @@ import lemon from "../../assets/img/hot_lemon.png";
 import milk from "../../assets/img/hot_milk.png";
 import matcha from "../../assets/img/hot_matcha.png";
 import { useState } from "react";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { ChevronLeft, ChevronRight, ColumnsSettings } from "lucide-react";
 const Menu = () => {
   const drinks = [
     { src: latte, name: "hot latte" },
@@ -17,7 +17,17 @@ const Menu = () => {
     { src: matcha, name: "hot matcha" },
   ];
 
-  const [index, setIndex] = useState(0);
+  let [index, setIndex] = useState(0);
+
+  if (index >= drinks.length - 1) {
+    setIndex(0);
+  }
+  const handleChevronLeft = () => {
+    setIndex((index) => index - 1);
+  };
+  const handleChevronRight = () => {
+    setIndex((index) => index + 1);
+  };
 
   return (
     <>
@@ -29,21 +39,27 @@ const Menu = () => {
       />
       <div className="mx-auto text-primary flex flex-col justify-center items-center ">
         <div className="items-center">
-          <img className="w-117.5" src={latte} alt="" />
+          <img
+            className="w-117.5"
+            src={drinks[index].src}
+            alt={drinks[index].name}
+          />
         </div>
-        <div className="w-171 flex items-center justify-center ">
+        <div className="w-180 flex items-center justify-between ">
           <ChevronLeft
             size={48}
             strokeWidth={3}
             color="#432818"
             className="cursor-pointer hover:scale-120 transition-all  duration-100"
+            onClick={handleChevronLeft}
           />
-          <p className="font-semibold text-[64px] mx-21.25">Hot latte</p>
+          <p className="font-medium text-[60px] ">{drinks[index].name}</p>
           <ChevronRight
             size={48}
             strokeWidth={3}
             color="#432818"
             className="cursor-pointer hover:scale-120 transition-all  duration-100"
+            onClick={handleChevronRight}
           />
         </div>
       </div>
