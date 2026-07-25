e4c79d3ca2b970bdd8313e045f4c92d8ec0fed55
wee08 <manutnithyasean@gmail.com>
2026-07-25 13:47:40 +0700

    built contact componente

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,19 @@ import Home from "./components/page/Home";
 import Story from "./components/page/Story";
 import Menu from "./components/page/Menu";
 import Location from "./components/page/Location";
+import Contact from "./components/page/Contact";
 const App = () => {
   const components = [
     { item: <Home />, id: "home" },
     { item: <Story />, id: "story" },
     { item: <Menu />, id: "menu" },
     { item: <Location />, id: "location" },
+    { item: <Contact />, id: "contact" },
   ];
 
   return (
     <>
-      <div className="mx-12.5 my-10.5 max-w-360 max-h-1877.25 font-mono ">
+      <div className="mx-12.5 my-10.5 max-w-360 min-h-1877.25 font-mono ">
         <div className="sticky top-0 overflow-hidden z-100">
           <Navbar />
         </div>
---
+++ src/components/page/Contact.jsx
@@ -0,0 +1,43 @@
+import pattern from "../../assets/img/footer_sitting.png";
+import Ig from "../../assets/svg/instagram-stroke-rounded.svg?react";
+import Fb from "../../assets/svg/square-f-stroke-rounded.svg?react";
+import Tt from "../../assets/svg/tiktok-stroke-rounded.svg?react";
+const Contact = () => {
+  const contacts = [
+    { icon: <Ig />, name: "iced_coffee" },
+    { icon: <Tt />, name: "iced_coffee" },
+    {
+      icon: <Fb />,
+      name: "iced_coffee",
+    },
+  ];
+  return (
+    <div className="relative my-12.5 w-full h-[320px] bg-primary rounded-[25px] overflow-hidden">
+      <div className="grid grid-cols-2 h-full grid-rows-1 overflow-hidden   ">
+        <div className="self-center ml-20 border-r border-white">
+          <div>
+            {contacts.map((item, idx) => (
+              <div key={idx} className="flex items-center gap-5 my-2">
+                <div>{item.icon}</div>
+                <p className="text-white text-nav">{item.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="self-center flex items-center bg-white w-[522px] h-[60px] rounded-full pl-12 mx-auto">
+          <input className="outline-none" type="text" placeholder="message" />
+          <button
+            className=" bg-btnprimary h-[61px] w-[141px] rounded-full text-white ml-auto"
+            type="button">
+            send
+          </button>
+        </div>
+      </div>
+      <div className="absolute bottom-0 inset-e-0 opacity-30">
+        <img className=" size-[191px] " src={pattern} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
