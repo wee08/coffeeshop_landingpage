fc68e79f329ea6b53c639941719176898d50ee59
wee08 <manutnithyasean@gmail.com>
2026-07-25 16:23:58 +0700

    ""

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), tailwindcss()],
+  plugins: [react(), svgr(), tailwindcss(), cloudflare()],
 });
